206a2dd1908477b4d48967ed7c31fdf43c2eb6f1
reynier25 <fsoto@uchicago.edu>
2019-07-31 14:21:46 -0700

    heroku prep

--- frontend/components/questions/question_index.jsx
+++ frontend/components/questions/question_index.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import QuestionItem from './question_item';
+// import React from 'react';
+// import QuestionItem from './question_item';
 
-class QuestionIndex extends React.component {
-    constructor(props) {
-        super(props);
-    }
+// class QuestionIndex extends React.component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    componentDidMount() {
-        this.props.fetchQuestions();
-    }
-
-
-    render() {
-        const { questions } = this.props;
-
-        return (
+//     componentDidMount() {
+//         this.props.fetchQuestions();
+//     }
 
 
-            
-        )
+//     render() {
+//         const { questions } = this.props;
+
+//         return (
+//             <div>
+//                 <ul>
+//                     {
+//                         questions.map(question => (
+//                             <QuestionItem
+//                                 key={`question${question.id}`}
+//                                 question={question} />
+//                             )
+//                         )
+//                     }
+//                 </ul>
+//             </div>
+//         )
+//     }
+
+// }
 
 
-    }
-
-}
-
-
-export default QuestionsIndex
+// export default QuestionIndex
---
+++ frontend/components/questions/question_item.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// export default ({ question }) => {
+
+//     //handle votes?
+
+//     return (
+//         <li>
+//             <h3>{question.title}</h3>
+//             <p>{question.body}</p>
+//             <br/>
+//         </li>
+//     )
+
+// }
