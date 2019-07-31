cdea9ccc8710e0b166424019fbc644b6c57c5701
reynier25 <fsoto@uchicago.edu>
2019-07-31 16:22:16 -0700

    syntax bugs

--- frontend/reducers/questions_reducer.js
+++ frontend/reducers/questions_reducer.js
@@ -8,10 +8,10 @@ export default (state = {}, action) => {
         case RECEIVE_QUESTION:
             return Object.assign({}, { [action.question.id]: action.question });
         case RECEIVE_QUESTIONS:
-            const questions = {}
+            const questions = {};
             action.questions.forEach(question => {
-                questions[question.id] = question
-            })
+                questions[question.id] = question;
+            });
             return questions;
         default:
             return state
