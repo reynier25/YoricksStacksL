653c68341a8dec23318f6c6fc8c22eb5a457cec3
reynier25 <fsoto@uchicago.edu>
2019-08-09 13:24:35 -0700

    asdf

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
     validates :name, presence: true
 
-    # has_and_belongs_to_many :questions, optional: true
+    has_and_belongs_to_many :questions, optional: true
 
     # attr_accessor :question_ids
 
